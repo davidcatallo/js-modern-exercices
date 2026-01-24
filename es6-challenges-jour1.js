3e63b13be7e9659b92c8175698731c851417289f
davidcat <deivecat98@gmail.com>
2026-01-24 15:16:10 +0100

    #

--- es6-challenges-jour1.js
+++ es6-challenges-jour1.js
@@ -54,5 +54,9 @@ const sum = (...args) => {
 };
 
 // 8. Use the Spread Operator to Evaluate Arrays In-Place
-const arr = [6, 89, 3, 45];
-const maximus = Math.max(...arr);
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2 
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
